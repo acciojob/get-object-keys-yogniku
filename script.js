44944850a9a8f973f067fc81184c2fbc053ce78b
yogniku <156093318+yogniku@users.noreply.github.com>
2024-03-11 23:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+// Create an object called student with a property called name
+let student = {
+  name: "jony"
+};
+
+// Add a property to the Object prototype called getKeys() that returns an array of all the keys in the object
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Test the getKeys() method on the student object
+console.log(student.getKeys()); // Output: ["name"]
